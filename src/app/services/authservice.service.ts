96f2e27135c0f0611c94780ac956be0ed191a90f
Dhruv Sehgal <dhruvsehgal007@gmail.com>
2025-12-24 13:36:41 +0530

    design changes

--- src/app/services/authservice.service.ts
+++ src/app/services/authservice.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class AuthserviceService {
-  // private baseUrl = 'http://localhost:3000/api/auth';
+  // private baseUrl = 'http://localhost:5000/api';
   private baseUrl = 'https://staging.ekarigar.com/tiaraapi/api';
 
   constructor(private http: HttpClient) {
